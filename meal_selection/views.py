a320a51fccaeba390f8820113fc7cf7f8ec5518f
Luca Maxwell <lucamaxwell@hotmail.co.uk>
2025-01-21 16:19:44 +0000

    switch to working branch

--- meal_selection/views.py
+++ meal_selection/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def selection(request):
-    return HttpResponse('allows patients or careers to select and confirm meals')
+    return HttpResponse('string')
