d36b6bc4f51f98a16e2530f52f38033349bd6fd3
Luca Maxwell <lucamaxwell@hotmail.co.uk>
2025-01-24 13:32:40 +0000

    Title logic for templates

--- meals/views.py
+++ meals/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, HttpResponse
 from .models import Meal
 
-
 def meal_view(request):
-    meals = Meal.objects.all()  # Retrieve all Meal objects, change to 'filter(author=1)' every user is assigned a PK, this would be the first user. 
-    return render(request, 'meals/meal_list.html', {'object_list': meals})
+    meals = Meal.objects.all()  # Retrieve all Meal objects, change to 'filter(author=1)' if you want to filter by author
+    context = {
+        'object_list': meals,
+        'title': 'Available Meals'
+    }
+    return render(request, 'meals/meal_list.html', context)
 
 def home(request):
-    return HttpResponse('This is my home page')
+    return render(request, 'meals/home.html')
