544554d84bb07288e733b469402a035a4d3a3b41
Luca Maxwell <lucamaxwell@hotmail.co.uk>
2025-01-27 13:16:47 +0000

    sorting templates

--- meal_selection/forms.py
+++ meal_selection/forms.py
@@ -4,4 +4,4 @@ from .models import MealSelection
 class MealSelectionForm(forms.ModelForm):
     class Meta:
         model = MealSelection
-        fields = ['meal', 'meal_type', 'confirmed']
+        fields = ['meal_type', 'meal', 'confirmed']
